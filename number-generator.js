173e17116740d3ae8d3ca2f75e60bf7fdf963cbf
berfutulgar <berfutulgar@gmail.com>
2024-10-08 20:42:54 +0300

    Added a JavaScript file to generate six random numbers

---
+++ number-generator.js
@@ -0,0 +1,14 @@
+function generateNumbers(){
+    return Math.ceil(Math.random()*60)
+}
+generateNumbers()
+
+var num1 = generateNumbers()
+var num2 = generateNumbers()
+var num3 = generateNumbers()
+var num4 = generateNumbers()
+var num5 = generateNumbers()
+var num6 = generateNumbers()
+
+
+console.log(num1 + ' ' + num2 + ' ' + num3 + ' ' + num4 + ' ' + num5 + ' ' + num6)
